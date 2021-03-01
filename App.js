bda9cd888c07697accd8226fe33868b383ecf593
samrat1223 <samratganguly37@gmail.com>
2021-03-01 18:11:08 +0530

    POST method added

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Item = require('./models/items');
-
 const app = express()
+
+app.use(express.urlencoded({extended:true}));
 const mongodb = 'mongodb+srv://Samrat:sam@cluster0.blkuc.mongodb.net/item-database?retryWrites=true&w=majority';
 mongoose.connect(mongodb,{useNewUrlParser:true , useUnifiedTopology: true }).then(() => {
     console.log('connected')
@@ -29,6 +30,14 @@ app.get('/get-items',(req,res) => {
 
 app.get('/add-item',(req,res) => {
     res.render('add-item');
+})
+
+app.post('/items',(req,res) => {
+    console.log(req.body)
+    const item = Item(req.body);
+    item.save().then(() => {
+        res.redirect('./get-items')
+    }).catch(err=>console.log(err))
 })
 
 app.use((req,res) => {
