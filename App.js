6e4ee332373642f696a04ae0d632c70843276ab8
samrat1223 <samratganguly37@gmail.com>
2021-03-02 20:00:18 +0530

    edit modal and PUT request added

--- App.js
+++ App.js
@@ -49,6 +49,22 @@ app.get('/items/:id',(req,res) => {
     })
 })
 
+app.delete('/items/:id',(req,res) => {
+    const id = req.params.id;
+    Item.findByIdAndDelete(id).then(result => {
+       res.json({redirect:'/get-items'})
+    })
+})
+
+app.put('/items/:id',(req,res) => {
+    const id = req.params.id;
+    Item.findByIdAndUpdate(id,req.body).then(result => {
+       res.json({msg:'Updated Successfully'})
+    })
+})
+
+
+
 app.use((req,res) => {
     res.render('error');
 })
